d8c56f628f1400dbdf10ec792a672548fa2e0c8f
Mattias Wiberg <mattiaswiberg@outlook.com>
2026-05-14 19:34:00 +0200

    feat: add grants snippet

---
+++ supabase/snippets/grants.sql
@@ -0,0 +1,3 @@
+GRANT USAGE ON SCHEMA guestbook TO anon, authenticated, service_role;
+GRANT ALL ON ALL TABLES IN SCHEMA guestbook TO anon, authenticated, service_role;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA guestbook TO anon, authenticated, service_role;
